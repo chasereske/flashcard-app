0ae59ce7a12f81984488259f823cff44b67e0aff
chasereske <chasereske@gmail.com>
2021-03-04 19:28:11 -0500

    Added both the put and the delete calls into the collections routes file. There is an error in the put call and I need to test the delete call.

--- routes/collections.js
+++ routes/collections.js
@@ -16,7 +16,7 @@ router.get('/:id', async (req, res) => {
         const collection = await Collection.findById(req.params.id);
 
         if(!collection)
-            return res.status(400).send(`The product with id "${req.params.id}" does not exist.`);
+            return res.status(400).send(`The collection with id "${req.params.id}" does not exist.`);
         
         return res.send(collection);
 
@@ -42,6 +42,47 @@ router.post('/', async (req, res) => {
         return res.send(collection);
 
     } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try{
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+        const collection = await Collection.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                description: req.body.description,
+                category: req.body.category,
+            },
+            { new: true }
+        );
+
+        if (!collection)
+            return res.status(400).send(`The collection with id "${req.params.id}" does not exist.`);
+
+            await collection.save();
+
+            return res.send(collection);
+    }       catch (ex) {
+            return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+
+        const collection = await Collection.findByIdAndRemove(req.params.id);
+
+        if (!collection)
+            return res.status(400).send(`The collection with id "${req.params.id}" does not exist.`);
+        
+        return res.send(collection);
+
+    }   catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
